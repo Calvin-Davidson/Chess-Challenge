a78b8ff77480392e309b53c34c64c65c213abb1e
Calvin-Davidson <calvin2003davidson@gmail.com>
2023-07-22 10:44:41 +0200

    Priority calculation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,35 @@
-﻿using ChessChallenge.API;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChessChallenge.API;
+
+namespace Chess_Challenge.My_Bot;
 
 public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
+        Random random = new Random();
         Move[] moves = board.GetLegalMoves();
-        return moves[0];
+
+        Dictionary<Move, int> moveStrengthMapper = new();
+  
+        foreach (var move in moves)
+        {
+            if (MoveIsCheckmate(board, move)) return move;
+            
+            moveStrengthMapper[move] = (int)move.CapturePieceType + ((int)move.MovePieceType * -1 + 6) / 2 + (move.IsPromotion ? 10 : 0);
+        }
+
+        var strongestMoveKeyValuePair = moveStrengthMapper.MaxBy(x => x.Value);
+        return strongestMoveKeyValuePair.Value == 0 ? moves[random.Next(0, moves.Length)] : strongestMoveKeyValuePair.Key;
+    }
+    
+    bool MoveIsCheckmate(Board board, Move move)
+    {
+        board.MakeMove(move);
+        bool isMate = board.IsInCheckmate();
+        board.UndoMove(move);
+        return isMate;
     }
 }
